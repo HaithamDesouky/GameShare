d94de76a443f77e5345e37952c5c7295153ac4e7
bbrolezzi <bbrolezzi@users.noreply.github.com>
2020-08-03 16:11:27 +0100

    models with Timestamps added

--- models/game.js
+++ models/game.js
@@ -1,36 +1,44 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const gameSchema = new mongoose.Schema({
-  creator: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
+const gameSchema = new mongoose.Schema(
+  {
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: true
+    },
+    date: {
+      type: Date,
+      required: true
+    },
+    content: {
+      type: String,
+      minlength: 3,
+      maxlength: 280,
+      required: true
+    },
+    picPath: {
+      type: String
+    },
+    picName: {
+      type: String
+    }
   },
-  name: {
-    type: String,
-    trim: true,
-    required: true
-  },
-  date: {
-    type: Date,
-    required: true
-  },
-  content: {
-    type: String,
-    minlength: 3,
-    maxlength: 280,
-    required: true
-  },
-  picPath: {
-    type: String
-  },
-  picName: {
-    type: String
+  {
+    timestamps: {
+      createdAt: 'creationDate',
+      updatedAt: 'editDate'
+    }
   }
   /*githubToken: {
     type: String
   }*/
-});
+);
 const Game = mongoose.model('Game', gameSchema);
 module.exports = mongoose.model('Game', schema);
--- models/user.js
+++ models/user.js
@@ -1,49 +1,57 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true
-  },
-  photo: {
-    type: String
-  },
-  email: {
-    type: String,
-    required: true,
-    lowercase: true,
-    trim: true
-  },
-  passwordHash: {
-    type: String
-  },
-  location: {
-    coordinates: [
-      {
-        type: Number,
-        min: -180,
-        max: 180
-      }
-    ],
-    type: {
+const userSchema = new mongoose.Schema(
+  {
+    name: {
       type: String,
-      default: 'Point'
+      trim: true
+    },
+    photo: {
+      type: String
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true
+    },
+    passwordHash: {
+      type: String
+    },
+    location: {
+      coordinates: [
+        {
+          type: Number,
+          min: -180,
+          max: 180
+        }
+      ],
+      type: {
+        type: String,
+        default: 'Point'
+      }
+    },
+    games: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Game' }],
+    status: {
+      type: String,
+      enum: ['pending_confirmation', 'active'],
+      default: 'pending_confirmation'
+    },
+    confirmationToken: {
+      type: String,
+      unique: true
     }
   },
-  games: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Game' }],
-  status: {
-    type: String,
-    enum: ['pending_confirmation', 'active'],
-    default: 'pending_confirmation'
-  },
-  confirmationToken: {
-    type: String,
-    unique: true
-  }
-  /*githubToken: {
+  {
+    timestamps: {
+      createdAt: 'creationDate',
+      updatedAt: 'editDate'
+    }
+    /*githubToken: {
     type: String
   }*/
-});
+  }
+);
 const User = mongoose.model('User', userSchema);
 module.exports = mongoose.model('User', schema);
