40fae3998132c9dc80adf706369f9577151f23c2
bbrolezzi <bbrolezzi@users.noreply.github.com>
2020-08-03 15:48:03 +0100

    models done

---
+++ models/game.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const gameSchema = new mongoose.Schema({
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  name: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  content: {
+    type: String,
+    minlength: 3,
+    maxlength: 280,
+    required: true
+  },
+  picPath: {
+    type: String
+  },
+  picName: {
+    type: String
+  }
+  /*githubToken: {
+    type: String
+  }*/
+});
+const Game = mongoose.model('Game', gameSchema);
+module.exports = mongoose.model('Game', schema);
