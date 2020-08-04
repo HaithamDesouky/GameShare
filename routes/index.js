898b1e3707f910ba0c3cdd642364c88ffb3fb885
onedweezy <64087376+onedweezy@users.noreply.github.com>
2020-08-04 13:50:36 +0100

    users are now shown on map and can be clicked on

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,28 @@ const { Router } = require('express');
 const router = new Router();
 const routeGuard = require('./../middleware/route-guard');
 const Game = require('../models/game');
+const User = require('../models/user');
 
 router.get('/', (req, res, next) => {
+  let games;
+  let users;
   Game.find()
-    .then(games => {
+    .then(data => {
       console.log(games);
-      res.render('index', { games });
+      games = data;
+    })
+    .catch(error => {
+      next(error);
+    });
+
+  User.find()
+    .then(file => {
+      users = file;
+    })
+    .then(() => {
+      console.log('here are the games', games);
+      console.log('here are the users', users);
+      res.render('index', { users, games });
     })
     .catch(error => {
       next(error);
