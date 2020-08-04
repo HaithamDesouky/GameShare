a6acdff82fc9ba327af99b36795e0b6927a1a775
bbrolezzi <bbrolezzi@users.noreply.github.com>
2020-08-04 12:47:41 +0100

    game info displaying on index view

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,17 @@
 const { Router } = require('express');
 const router = new Router();
 const routeGuard = require('./../middleware/route-guard');
+const Game = require('../models/game');
 
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Hello World!' });
+  Game.find()
+    .then(games => {
+      console.log(games);
+      res.render('index', { games });
+    })
+    .catch(error => {
+      next(error);
+    });
 });
 
 module.exports = router;
