b95730f0c022dc7711bd679e0ea363096a993078
asxisto <asxisto@gmail.com>
2020-08-06 15:43:15 +0100

    email notification

--- routes/gameRouter.js
+++ routes/gameRouter.js
@@ -13,42 +13,32 @@ const storage = new multerStorageCloudinary.CloudinaryStorage({
 });
 const upload = multer({ storage });
 
-<<<<<<< HEAD
-=======
-gameRouter.post(
-  '/create',
-  upload.single('photo'),
-  routeGuard,
-  (req, res, next) => {
-    const { name, date, content, platform } = req.body;
-    const id = res.locals.user._id;
-    let photoUpload;
-    !req.file
-      ? (photoUpload =
-          'https://res.cloudinary.com/asxisto/image/upload/v1596535816/gamechanger/default_game.png')
-      : (photoUpload = req.file.path);
-    console.log(photoUpload);
-    Game.create({
-      creator: id,
-      name,
-      date,
-      content,
-      photo: photoUpload,
-      platform
-    })
-      .then(game => {
-        return User.findByIdAndUpdate(id, {
-          $push: { games: game._id }
-        });
-      })
-      .then(() => {
-        res.redirect('/profile');
-      })
-      .catch(error => {
-        next(error);
+gameRouter.post('/create', upload.single('photo'), routeGuard, (req, res, next) => {
+  const { name, date, content, platform } = req.body;
+  const id = res.locals.user._id;
+  let photoUpload;
+  !req.file ? (photoUpload = 'https://res.cloudinary.com/asxisto/image/upload/v1596535816/gamechanger/default_game.png') : (photoUpload = req.file.path);
+  console.log(photoUpload);
+  Game.create({
+    creator: id,
+    name,
+    date,
+    content,
+    photo: photoUpload,
+    platform
+  })
+    .then(game => {
+      return User.findByIdAndUpdate(id, {
+        $push: { games: game._id }
       });
-  }
-);
+    })
+    .then(() => {
+      res.redirect('/profile');
+    })
+    .catch(error => {
+      next(error);
+    });
+});
 
 gameRouter.get('/platform', routeGuard, (req, res, next) => {
   const platformInput = req.query.platform;
@@ -63,7 +53,6 @@ gameRouter.get('/platform', routeGuard, (req, res, next) => {
     });
 });
 
->>>>>>> a81abff12634678fb9f4dbd4ccb79ebcb6ea4a87
 gameRouter.get('/search', (req, res, next) => {
   const { platform, name } = req.query;
   let query = {};
@@ -117,11 +106,7 @@ gameRouter.get('/:id/delete', routeGuard, (req, res, next) => {
   const userId = req.session.passport.user;
   console.log(id, userId);
 
-  User.findByIdAndUpdate(
-    { _id: userId },
-    { $pull: { games: id } },
-    { safe: true, upsert: true }
-  ).then(() => {
+  User.findByIdAndUpdate({ _id: userId }, { $pull: { games: id } }, { safe: true, upsert: true }).then(() => {
     Game.findOneAndDelete({ _id: id, creator: userId })
       .then(data => {
         console.log(data);
@@ -150,38 +135,33 @@ gameRouter.get('/:id/edit', routeGuard, (req, res, next) => {
     });
 });
 
-gameRouter.post(
-  '/:id/edit',
-  upload.single('photo'),
-  routeGuard,
-  (req, res, next) => {
-    const id = req.params.id;
-    const userId = req.session.passport.user;
-    const { name, date, content, platform } = req.body;
-    let newGamePhoto;
+gameRouter.post('/:id/edit', upload.single('photo'), routeGuard, (req, res, next) => {
+  const id = req.params.id;
+  const userId = req.session.passport.user;
+  const { name, date, content, platform } = req.body;
+  let newGamePhoto;
 
-    let data;
-    if (!req.file) {
-      data = {
-        name,
-        date,
-        content,
-        platform
-      };
-    } else {
-      newGamePhoto = req.file.path;
-      data = { name, date, content, platform, photo: newGamePhoto };
-    }
-
-    Game.findOneAndUpdate({ _id: id, creator: userId }, data)
-      .then(() => {
-        res.redirect('/profile');
-      })
-      .catch(error => {
-        next(error);
-      });
+  let data;
+  if (!req.file) {
+    data = {
+      name,
+      date,
+      content,
+      platform
+    };
+  } else {
+    newGamePhoto = req.file.path;
+    data = { name, date, content, platform, photo: newGamePhoto };
   }
-);
+
+  Game.findOneAndUpdate({ _id: id, creator: userId }, data)
+    .then(() => {
+      res.redirect('/profile');
+    })
+    .catch(error => {
+      next(error);
+    });
+});
 
 gameRouter.get('/platform', routeGuard, (req, res, next) => {
   const platformInput = req.query.platform;
